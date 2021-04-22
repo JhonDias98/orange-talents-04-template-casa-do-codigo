6beba5c87c1d02ee15049e317c8657d6212aa1a8
Jonathan Pereira Dias <52224050+JhonDias98@users.noreply.github.com>
2021-04-22 17:47:12 -0300

    Implementado o cadastro do livro

---
+++ src/main/java/br/com/zupacademy/jonathan/casadocodigo/livro/Livro.java
@@ -0,0 +1,106 @@
+package br.com.zupacademy.jonathan.casadocodigo.livro;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import br.com.zupacademy.jonathan.casadocodigo.autor.Autor;
+import br.com.zupacademy.jonathan.casadocodigo.categoria.Categoria;
+
+@Entity
+public class Livro {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotBlank
+	private String titulo;
+	@NotBlank
+	@Size(max = 500)
+	private String resumo;
+	private String sumario;
+	@NotNull
+	@Min(20)
+	private BigDecimal preco;
+	@NotNull
+	@Min(100)
+	private Integer numeroPaginas;
+	@NotBlank
+	private String isbn;
+	@Future
+	@JsonFormat(pattern = "dd/MM/yyyy", shape = JsonFormat.Shape.STRING)
+	private LocalDate dataPublicacao;
+	@ManyToOne
+	private Categoria categoria;
+	@ManyToOne
+	private Autor autor;
+	
+	public Livro(@NotBlank String titulo, @NotBlank @Size(max = 500) String resumo, String sumario,
+			@NotNull @Min(20) BigDecimal preco, @NotNull @Min(100) Integer numeroPaginas, @NotBlank String isbn,
+			@Future LocalDate dataPublicacao, Categoria categoria, Autor autor) {
+		this.titulo = titulo;
+		this.resumo = resumo;
+		this.sumario = sumario;
+		this.preco = preco;
+		this.numeroPaginas = numeroPaginas;
+		this.isbn = isbn;
+		this.dataPublicacao = dataPublicacao;
+		this.categoria = categoria;
+		this.autor = autor;
+	}
+
+	@Deprecated
+	public Livro() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public String getResumo() {
+		return resumo;
+	}
+
+	public String getSumario() {
+		return sumario;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public Integer getNumeroPaginas() {
+		return numeroPaginas;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public LocalDate getDataPublicacao() {
+		return dataPublicacao;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public Autor getAutor() {
+		return autor;
+	}
+}
---
+++ src/main/java/br/com/zupacademy/jonathan/casadocodigo/livro/LivroRequest.java
@@ -0,0 +1,104 @@
+package br.com.zupacademy.jonathan.casadocodigo.livro;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.EntityManager;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.util.Assert;
+
+import br.com.zupacademy.jonathan.casadocodigo.autor.Autor;
+import br.com.zupacademy.jonathan.casadocodigo.categoria.Categoria;
+import br.com.zupacademy.jonathan.casadocodigo.config.validation.UniqueValue;
+
+public class LivroRequest {
+	@NotBlank
+	@UniqueValue(domainClass = Livro.class, fieldName = "titulo", message = "Já existe um titulo com esse nome")
+	private String titulo;
+	@NotBlank
+	@Size(max = 500)
+	private String resumo;
+	private String sumario;
+	@NotNull
+	@Min(20)
+	private BigDecimal preco;
+	@NotNull
+	@Min(100)
+	private Integer numeroPaginas;
+	@NotBlank
+	@UniqueValue(domainClass = Livro.class, fieldName = "isbn", message = "O Isbn informado já existe")
+	private String isbn;
+	@Future
+	@JsonFormat(pattern = "dd/MM/yyyy", shape = JsonFormat.Shape.STRING)
+	private LocalDate dataPublicacao;
+	@NotNull
+	private Long categoriaId;
+	@NotNull
+	private Long autorId;
+	
+	public LivroRequest(@NotBlank String titulo, @NotBlank @Size(max = 500) String resumo, String sumario,
+			@NotNull @Min(20) BigDecimal preco, @NotNull @Min(100) Integer numeroPaginas, @NotBlank String isbn,
+			@Future LocalDate dataPublicacao, @NotNull Long categoriaId, @NotNull Long autorId) {
+		this.titulo = titulo;
+		this.resumo = resumo;
+		this.sumario = sumario;
+		this.preco = preco;
+		this.numeroPaginas = numeroPaginas;
+		this.isbn = isbn;
+		this.dataPublicacao = dataPublicacao;
+		this.categoriaId = categoriaId;
+		this.autorId = autorId;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public String getResumo() {
+		return resumo;
+	}
+
+	public String getSumario() {
+		return sumario;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public Integer getNumeroPaginas() {
+		return numeroPaginas;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public LocalDate getDataPublicacao() {
+		return dataPublicacao;
+	}
+
+	public Long getCategoriaId() {
+		return categoriaId;
+	}
+
+	public Long getAutorId() {
+		return autorId;
+	}
+	
+	public Livro toModel(EntityManager manager) {
+		Categoria categoria = manager.find(Categoria.class, categoriaId); // 1
+        Autor autor = manager.find(Autor.class, autorId); // 1
+
+        Assert.state(autor!=null, "Autor(a) não encontrado(a) para o id " + autorId);
+        Assert.state(categoria!=null, "Categoria não encontrado para o id " + categoriaId);
+		
+		return new Livro(this.titulo, this.resumo, this.sumario, this.preco, this.numeroPaginas, this.isbn, this.dataPublicacao, categoria, autor);
+	}
+}
